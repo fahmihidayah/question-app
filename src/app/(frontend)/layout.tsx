ec9ce6e76f794535961797ca769ec1059f721bbd
fahmi-mac <vambex.system@gmail.com>
2025-11-15 04:35:26 +0700

    update open grapsh

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -4,12 +4,14 @@ import { QueryProvider } from '@/providers/QueryProvider'
 import { ThemeProvider } from '@/providers/ThemeProvider'
 import { Navbar } from '@/components/layouts/navbar'
 import { Metadata } from 'next'
+import { mergeOpenGraph } from '@/utilities/mergeOpenGraph'
 
 
 export async function generateMetadata(): Promise<Metadata> {
     return {
         title : "KonfQ - Platform Tanya Jawab Konferensi",
-        description : "Platform interaktif untuk tanya jawab konferensi yang memungkinkan audiens berpartisipasi aktif dalam diskusi"
+        description : "Platform interaktif untuk tanya jawab konferensi yang memungkinkan audiens berpartisipasi aktif dalam diskusi",
+        openGraph : mergeOpenGraph()
     }
 }
 export default async function RootLayout(props: { children: React.ReactNode }) {
